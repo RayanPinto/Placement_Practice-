89b2c5ae96cbf50a87421fe55d5aa49d9019f8e0
Rayan Pinto <rayanpinto9482@gmail.com>
2025-10-07 20:44:28 +0530

    update files

--- heapsort.py
+++ heapsort.py
@@ -1,1 +1,21 @@
-def 
+def heapify(arr,n,i):
+    largest=i
+    l=i*2+1
+    r=i*2+2
+    if l<n and arr[l]>arr[largest]:
+        largest=l
+    if r<n and arr[r]>arr[largest]:
+        largest=r
+    if largest!=i:
+        arr[i],arr[largest]=arr[largest],arr[i]
+        heapify(arr,n,largest)
+def heapsort(arr):
+    n=len(arr)
+    for i in range(n//2-1,-1,-1):
+        heapify(arr,n,i)
+    for i in range(n-1,0,-1):
+        arr[0],arr[i]=arr[i],arr[0]
+        heapify(arr,i,0)
+    return arr
+arr=[1,5,42,1,3,45,33,23,444,4]
+print(heapsort(arr))
