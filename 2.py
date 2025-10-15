1fac9a52ccac9996401d9bfa058f323c83fb6851
Rayan Pinto <rayanpinto9482@gmail.com>
2025-10-15 22:51:04 +0530

    update files

--- 2.py
+++ 2.py
@@ -1,6 +1,20 @@
-arr=[1,2,3,4]
+from collections import deque
+queue=deque([(5,0)])
+visited=set()
+while queue:
+    x,y=queue.popleft()
+    if x==0:
+        print("we did it")
+        break
+    if y in visited:
+        continue
+    visited.add(y)
+    print(visited)
+    queue.append((x-1,y+1))
+    print(queue)
+    queue.append((x//2,y+1))
+    print(queue)
+    queue.append((x-(2*x/2),y+1))
+    print(queue)
+    
 
-x,y,*a=arr
-if len(a)==2:
-    q,w=a
-print(q,w)
