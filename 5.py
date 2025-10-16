471a3e4ea6091027494ef1cbeb3863f21e29b4d9
Rayan Pinto <rayanpinto9482@gmail.com>
2025-10-16 22:51:38 +0530

    update files

---
+++ 5.py
@@ -0,0 +1,41 @@
+import re
+from tkinter import messagebox
+import tkinter as tk
+def validate_inputs():
+    name=ne.get().strip()
+    email=ee.get().strip()
+    password=pe.get().strip()
+    if not name:
+        messagebox.showerror("Error","Name cannot be empty")
+        return
+    if not email:
+        messagebox.showerror("Error","Email cannot be empty")
+        return 
+    if not password:
+        messagebox.showerror("Error", "Password cannot be empty")
+        return
+    email_pattern=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0]{2,}$'
+    if not re.match(email,email_pattern):
+        messagebox.showerror("Error","invalid email found")
+        return
+    if not any(char.isdight() for char in password):
+        messagebox.showerror("Error","password should have atleast 1 digit")
+        return 
+    if not any(char.isupper() for char in password):
+        messagebox.showerror("Error","Password must atleast an uppercase letter ")
+        return
+    if len(password)<8:
+        messagebox.showerror("Error","password must contain 8 characters")
+        return 
+    messagebox.showinfo("Success","form submitted ")
+root=tk.Tk()
+root.title("User registration form")
+root.geometry("400x300")
+tk.Label(root,text="Name:",font=("Arial",12)).pack(pady=5)
+ne=tk.Entry(root,font=("Arial",12)).pack(pady=5)
+tk.Label(root,text="Email:",font=("Arial",12)).pack(pady=5)
+pe=tk.Entry(root,font=("Arial",12)).pack(pady=5)
+tk.Label(root,text="Password:",font=("Arial",12)).pack(pady=5)
+ee=tk.Entry(root,font=("Arial",12)).pack(pady=5)
+submit=tk.Button(root,text="Submit",command=validate_inputs,font=("Arial",12)).pack(pady=5)
+root.mainloop()
--- n_queen.py
+++ n_queen.py
@@ -1,31 +1,26 @@
 def n_queens(n):
-    solutions=[]
-    cols=set()
+    board=["."*n for _ in range(n)]
+    print(board)
     drag1=set()
     drag2=set()
-    board=['.'*n for _ in range(n)]
-    print(board)
-    def place_queen(row):
+    cols=set()
+    solutions=[]
+    def place(row):
         if row==n:
             solutions.append(board.copy())
-            print(solutions)
             return 
         for col in range(n):
             if col in cols or (row-col) in drag1 or (row+col) in drag2:
                 continue
-            cols.add(col);drag1.add(row-col);drag2.add(row+col)
-            board[row]=board[row][:col]+'Q'+board[row][col+1:]
-            place_queen(row+1)
-            cols.remove(col);drag1.remove(row-col);drag2.remove(row+col)
-            board[row]=board[row][:col]+'.'+board[row][col+1:]
-    place_queen(0)
+            cols.add(col);drag1.add(row-col);drag2.add(row+col);
+            board[row]=board[row][:col]+"Q"+board[row][col+1:]
+            place(row+1)
+            cols.remove(col);drag1.remove(row-col);drag2.remove(row+col);
+            board[row]=board[row][:col]+"."+board[row][col+1:]
+    place(0)
     return solutions
-    
-n=4
-sols=n_queens(n)
+sols=n_queens(4)
 print(sols)
-for i,sol in enumerate(sols,1):
-    print(f"number of solutions :{len(sols)}")
-    print("\n".join(sol))
-    print()
-    
+for i,j in enumerate(sols,1):
+    print(f"Number of solution is {i}")
+    print("\n".join(j))
