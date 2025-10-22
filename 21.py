f746a9071f135c5539ce34647dee7280a506be77
Rayan Pinto <rayanpinto9482@gmail.com>
2025-10-22 21:57:53 +0530

    update files

--- 21.py
+++ 21.py
@@ -1,21 +1,19 @@
-arr=[4,5,4,5,2,5,6,5,6,5,6]
+arr=[1,2,1,3,1,2,3]
 n=len(arr)
-max_len=0
+freq={}
 k=2
 left=0
-freq={}
+maxlen=float('-inf')
 for i in range(n):
     freq[arr[i]]=freq.get(arr[i],0)+1
-    print(freq)
     while len(freq)>k:
         freq[arr[left]]-=1
-        
         if freq[arr[left]]==0:
             del freq[arr[left]]
         left+=1
-    if len(freq)==k:
-        if i-left+1>max_len:
-            max_len=i-left+1
+    if len(freq)<=k:
+        if i-left+1 > maxlen:
+            maxlen=i-left+1
             a=left
             b=i
-print(arr[a:b+1],a,b)
+print(arr[left:i+1])
