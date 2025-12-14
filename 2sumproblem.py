c559e62f5a55a8d445fffda38332b9032cf45cc5
Rayan Pinto <rayanpinto9482@gmail.com>
2025-12-15 00:30:23 +0530

    learnedSomethingNewToday

--- 2sumproblem.py
+++ 2sumproblem.py
@@ -1,8 +1,9 @@
 def twosum(arr,target):
     res={}
-    for i,num in enumerate(arr):
-        diff=target-num
+    for i,j in enumerate(arr):
+        diff=target-j
         if diff in res:
-            return res[diff],i
-        res[num]=i
+            return (res[diff],i)
+        res[j]=i
+    return (-1,-1)
 print(twosum(arr=[1,4,5,6,7,2,7],target=9))
